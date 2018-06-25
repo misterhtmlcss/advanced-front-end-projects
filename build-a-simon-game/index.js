94bd9e2b9fb91c688fafc4fce951869409111e7d
Roger K <misterhtmlcss@gmail.com>
2018-06-25 10:52:09 -0600

    Startup files for collaboration with Teo from Chingu for Simon Says game

---
+++ build-a-simon-game/index.js
@@ -0,0 +1,31 @@
+/*
+LIVE Pair Programmed with Teo.... & Roger Kondrat according to the FCC Academic Honesty rules:
+https://www.freecodecamp.org/academic-honesty
+
+Youtube Instructions Used from the Legacy Certificate:
+https://www.youtube.com/watch?v=tAPut8a47bA
+*/
+
+//User Story: I am presented with a random series of button presses.
+//User Story: If I press the wrong button, I am notified that I have done so, and that series of button presses starts again to remind me of the pattern so I can try again.
+
+/*
+Features
+Game:
+- Need Holding array for game
+- Need to reset game
+- Need to keep track of the users 'level' of play
+
+AI
+- Random Number Generator for 'AI' to select keys
+- No same select as last key function ex. AI doesn't select blue again for the next choice. We don't have to do that, but it seems like a good add.
+
+User
+- User clicks start button to begin game
+- User clicks reset button to end current game and remove score
+
+- User on first round gets first 3 colours
+- User receives a new key each round added to the last round
+-
+
+*/
