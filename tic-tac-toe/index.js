cc4386206cdbee22a071ce11e9a7e8a3570a3bed
Roger K <misterhtmlcss@gmail.com>
2018-08-07 13:58:49 -0600

    Finished visual design. I will move on to finish the js now

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,9 +1,7 @@
+'use strict';
 /*
-
 User Story: I can play a game of Tic Tac Toe with the computer.
-
 User Story: My game will reset as soon as it's over so I can play again.
-
 User Story: I can choose whether I want to play as X or O.
 
 // My decisions:
@@ -15,18 +13,103 @@ User Story: I can choose whether I want to play as X or O.
 
 So the user will select 0 and I'll select a random corner or 4 (always 4 if it's available) and if the user selects 1 or 2 then I'll select the remainder
 
-  [1, 2, 3]
-  [4, 5, 6]
-  [7, 8, 9]
-
 */
-let playerOne, playerAi;
-let boardh = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-let boardv =[[1, 4, 7], [2, 5, 8], [3, 6, 9]];
+// Game start from 0
+const startGame = 0
+// The max number of rounds per game
+const maxPlays = 8
+// Game board layout
+const setupBoard = [[0,1,2], [3,4,5], [6,7,8]];
+// Winning combinations
+const winningCombo = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]]
+// Variables for mutation
+let playerOne, playerAi, gameBoard;
+
+/* Update Span for Possible Moves/Rounds Remaining */
+const round = document.getElementById("round");
+const span = round.getElementsByTagName("span");
+
+const boxes = Array.from(document.querySelectorAll('.grid-item'))
+const resetBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let playerOneScore, playerAiScore, found;
+let gameRound = 0;
+let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // Change values for Selection
+let roundWonBy = 0 // Who won the round; Tie:0, HumanWin:1, AIWin: -1
+let winner = false; // After 3 rounds; overall winner is declared
+let tempClickValue; // Value of last user selection goes here.
+
+
+// Detect a user click then record into an array which square
+// Check a boxes that are clicked
+boxes.forEach(box => { box.addEventListener('click',getSelectionValue)})
+
+// Value of box selected
+function getSelectionValue (val){
+  tempClickValue = val.target.dataset.square // Click data- value
+  if(val.isTrusted === true){
+    testCall(`Player clicked this value: ${tempClickValue}`)
+  } else {
+    testCall(`Computer clicked this value: ${randomNumber()}`)
+  }
+}
+
+// AI Functions
+
+
+// This function needs to iterate through both it's previous selections for the AI and the user selections and then resolve a choice.
+function aiBoxPicker () {
+  //if()
+}
+
+// Game tools
+function keepScore () {  // Wins collected after each round
+  if (gameRound > 0 || gameRound <=5) {
+// For this function it receives the winer identity and then increments based on that identity
+    round.innerHTML = //`${***WINNER e.g. Player or AI}: ${playerOneScore++|| playerAiScore++}`
+  }
+}
+
+function roundWinner () { // Calls both keepScore and trackRounds to increment their counters
+  if (winner) {
+    trackRounds()
+  }
+}
+
+function trackRounds () { // Keep track of the round being played
+  if (gameRound > 0 || gameRound <=5) {
+    round.innerHTML = `Round: ${gameRound++}`;
+  }
+}
+// Generates Computer moves
+function randomNumber(){
+  let choice = Math.floor(Math.random() * Math.floor(10))
+
+
+  found = board.find(function(choice) {
+    if (board[choice] === !NaN) {
+      console.log("found a number")
+    }
+});
 
 
 
+  if (choice === )
+  return ;
+}
 
+// Utility Scripts for testing
+
+function testCall (...args) {
+  console.log(...args)
+}
 /*
 function winning(board, player){
 if (
@@ -41,7 +124,7 @@ if (
   )
   { return true; } else
   { return false; }
-
+}
 
 // Mike's code
 if (
@@ -53,6 +136,6 @@ if (
     (board[0][2] === symbol && board[1][2] === symbol && board[2][2] === symbol) ||
     (board[0][0] === symbol && board[1][1] === symbol && board[2][2] === symbol) ||
     (board[0][2] === symbol && board[1][1] === symbol && board[2][0] === symbol)
-  )
+  );
 
 */
