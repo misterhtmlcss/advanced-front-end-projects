95839e31b2cb27585a422d11467f45d316426658
Roger K <misterhtmlcss@gmail.com>
2018-08-08 21:16:40 -0600

    updated a couple small nuances on .html file, recompiled the .scss file and added a state management object to index.js file

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -12,10 +12,7 @@ User Story: I can choose whether I want to play as X or O.
 3. if user chooses any combination that includes 2 number from a value combination Array (I think or I may go Map; not sure yet) ex [0, [0,1,2]] then I'll take the remaining position that isn't filled.
 
 So the user will select 0 and I'll select a random corner or 4 (always 4 if it's available) and if the user selects 1 or 2 then I'll select the remainder
-
 */
-// Game start from 0
-const startGame = 0
 // The max number of rounds per game
 const maxPlays = 8
 // Game board layout
@@ -31,107 +28,104 @@ const winningCombo = [
   [0,4,8],   // top left to bottom right win
   [2,4,6]    // top right to bottom left win
 ]
-
-// Remaining Turns Counter
-const turns = document.getElementById("round");
-const span = turns.getElementsByTagName("span");
+// Radio buttons
+const radioBtns = document.getElementById('menu')
+// Setup Start Button
+const startBtn = document.getElementById('btn')
+// Remaining Choices Counter
+const turnSpan = document.getElementsByTagName("span");
+// Collect all boxes into an array
+const boxes = Array.from(document.querySelectorAll('.grid-item'))
+const container = document.querySelector('.grid-container')
 
 // Variables for mutation
-let playerOne, gameAi, gameState, gameBoard, gameRound, tempClickValue;
-/* Update Span for Possible Moves/Rounds Remaining */
+let playerOne, gameAi, gameBoard, gameRound, tempClickValue;
 
-// User needs to select a letter X or O
-//const menu = document.getElementById('menu')
-//const input = document.querySelectorAll('input[name="group"]:checked')//.value
-
-// Event listner to start the game
-const startBtn = document.getElementById('btn')
-startBtn.addEventListener('click', captureUserSelection)
 
 
 // User needs to then click start for game to begin.
-function init () {
-  gameState = true
+const gameState = {
+  state: false,
+  gameOver: false,
+  checkState: function(){
+    if(this.state === true && this.gameOver === true){
+      reset()
+      console.log(this.state, this.gameOver);
+    } else {
+      init()
+      console.log(this.state, this.gameOver);
+    }
+  },
 }
 
-//input.addEventListener('click', captureUserSelection) // Don't need!! Collect user selection on game start
-function captureUserSelection(input) {
+function loadListners() {
+  container.classList.toggle('disable')
+  // Check a boxes that are clicked
+  boxes.forEach(box => box.addEventListener('click',getSelectionValue, false))
+  startBtn.addEventListener('click', init, false)
+
+}
+
+
+function init () {
+  gameState.state = true // Set to true at start of game.
+  startBtn.textContent = 'Start'
+  container.classList.remove('disable')
+  boxes.forEach(box => {
+    box.innerText = ''
+  })
+  //console.log(boxes[0]);
+  radioBtns.style.display = 'none'
+  captureUserSelection()
+  console.log('init')
+}
+
+// Value of box selected
+function getSelectionValue (val){
+  tempClickValue = val.target.dataset.square // stores data- value
+  if(val.isTrusted === true){
+    boxes[tempClickValue].textContent = playerOne
+    console.log(`Player clicked this value: ${tempClickValue}`)
+  } else {
+    console.log(`Computer clicked this value: ${randomNumber()}`)
+  }
+}
+
+// User needs to select a letter X or O
+function captureUserSelection(gameOnConditions) {
   playerOne = document.querySelector('input[name="group"]:checked').value
   if(playerOne === 'x'){
     gameAi = 'y'
   } else {
     gameAi = 'x'
   }
-  console.log(playerOne, gameAi)
-  init(playerOne)
-  return
 }
 
-
-
-/*
-
-// Value of box selected
-function getSelectionValue (val){
-  tempClickValue = val.target.dataset.square // Click data- value
-  if(val.isTrusted === true){
-    testCall(`Player clicked this value: ${tempClickValue}`)
-  } else {
-    testCall(`Computer clicked this value: ${randomNumber()}`)
-  }
-}
-
-// AI Functions
-
-
-// This function needs to iterate through both it's previous selections for the AI and the user selections and then resolve a choice.
-function aiBoxPicker () {
-  //if()
-}
-
-// Game tools
-function keepScore () {  // Wins collected after each round
-  if (gameRound > 0 || gameRound <=5) {
-// For this function it receives the winer identity and then increments based on that identity
-    round.innerHTML = //`${***WINNER e.g. Player or AI}: ${playerOneScore++|| playerAiScore++}`
-  }
-}
-
-function roundWinner () { // Calls both keepScore and trackRounds to increment their counters
-  if (winner) {
-    trackRounds()
-  }
-}
-
-function trackRounds () { // Keep track of the round being played
-  if (gameRound > 0 || gameRound <=5) {
-    round.innerHTML = `Round: ${gameRound++}`;
-  }
-}
 // Generates Computer moves
 function randomNumber(){
-  let choice = Math.floor(Math.random() * Math.floor(10))
+  return Math.floor(Math.random() * Math.floor(10))
+}
 
-  found = board.find(function(choice) {
-    if (board[choice] === !NaN) {
-      console.log("found a number")
-    }
+window.addEventListener("load", loadListners);
+
+/*
+// ***** TESTING OUT OBSERVERS ***** //
+var observer = new MutationObserver(function(mutations) {
+	// For the sake of...observation...let's output the mutation to console to see how this all works
+	mutations.forEach(function(mutation) {
+    console.log(mutation.type, mutation);
+	});
 });
 
-}
+// Notify me of everything!
+var observerConfig = {
+	attributes: true,
+	childList: true,
+	characterData: true
+};
 
-// Utility Scripts for testing
-
-function testCall (...args) {
-  console.log(...args)
-}
-
-// Collect all boxes into an array
-const boxes = Array.from(document.querySelectorAll('.grid-item'))
-// Detect a user click then record into an array which square
-// Check a boxes that are clicked
-boxes.forEach(box => { box.addEventListener('click',getSelectionValue)})
-
-
-
+// Node, config
+// In this case we'll listen to all changes to body and child nodes
+var targetNode = document.gameState
+observer.observe(targetNode, observerConfig);
 */
